36df4b13649489117cdf48cbee8ac02e8533a325
Arafat <arafatulislamtc@gmail.com>
2024-06-04 04:05:10 +0600

    Verify Otp and issue jwt password reset token

--- Pos_Project/bootstrap/app.php
+++ Pos_Project/bootstrap/app.php
@@ -16,7 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens( except: [
             'http://127.0.0.1:8000/user-registration',
            'http://127.0.0.1:8000/user-login',
-           'http://127.0.0.1:8000/sent-otp'
+           'http://127.0.0.1:8000/sent-otp',
+           'http://127.0.0.1:8000/verify-otp',
+           
         ]);
 
         $middleware->alias([
