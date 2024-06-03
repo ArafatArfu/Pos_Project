8ae5187da1b0a1007b0b5f6b3e0bed8079cdb1be
Arafat <arafatulislamtc@gmail.com>
2024-06-04 01:12:36 +0600

    Jwt for Authentication,User login And Issue JWT Token

--- Pos_Project/bootstrap/app.php
+++ Pos_Project/bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         
         $middleware->validateCsrfTokens( except: [
             'http://127.0.0.1:8000/user-registration',
+           'http://127.0.0.1:8000/user-login'
         ]);
 
         $middleware->alias([
--- Pos_Project/routes/web.php
+++ Pos_Project/routes/web.php
@@ -5,3 +5,4 @@ use App\Http\Middleware\UserMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/user-registration',[UserController::class,'UserRegistration']);
+Route::post('/user-login',[UserController::class,'UserLogin']);
